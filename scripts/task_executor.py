d0387316222c95b3b6b7b568a1af8325c7104692
Fr0stM0urne <ibrahim.gr88@gmail.com>
2024-04-06 08:23:02 +0500

    change OPENAI_API_KEY from environ

--- scripts/task_executor.py
+++ scripts/task_executor.py
@@ -23,7 +23,7 @@ configs = load_config()
 
 if configs["MODEL"] == "OpenAI":
     mllm = OpenAIModel(base_url=configs["OPENAI_API_BASE"],
-                       api_key=configs["OPENAI_API_KEY"],
+                       api_key=os.environ["OPENAI_API_KEY"],
                        model=configs["OPENAI_API_MODEL"],
                        temperature=configs["TEMPERATURE"],
                        max_tokens=configs["MAX_TOKENS"])
